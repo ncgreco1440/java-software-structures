bc80b1ccb905c38e9391ae9a01582d915a7d4278
Nico Greco <ncgreco1440@gmail.com>
2020-02-28 18:43:53 -0500

    DropOutStack implementation and tests

--- src/main/java/com/nicogreco/structures/stack/DropOutStack.java
+++ src/main/java/com/nicogreco/structures/stack/DropOutStack.java
@@ -5,6 +5,92 @@ package com.nicogreco.structures.stack;
  * continue to push elements the first item[s] pushed onto the
  * Stack will become overwritten.
  */
-public class DropOutStack {
+public class DropOutStack<T> implements StackADT<T> {
+  private static final int DEFAULT_CAPACITY = 10;
+  private Node<T> head;
+  private Node<T> current;
+  private int size;
+  private int capacity;
 
+  private class Node<T> {
+    private T element;
+    private Node<T> prev;
+
+    public Node() {
+      this(null, null);
+    }
+
+    public Node(T element) {
+      this(element, null);
+    }
+
+    public Node(T element, Node<T> prev) {
+      this.element = element;
+      this.prev = prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+      this.prev = prev;
+    }
+
+    public void setElement(T elem) {
+      this.element = elem;
+    }
+
+    public Node<T> prev() {
+      return this.prev;
+    }
+  }
+
+  public DropOutStack() {
+    this(DEFAULT_CAPACITY);
+  }
+
+  public DropOutStack(int capacity) {
+    this.capacity = capacity;
+  }
+
+  public int capacity() {
+    return this.capacity;
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  public void push(T element) {  
+    if (this.isEmpty()) {
+      this.head = new Node<T>(element);
+      this.current = this.head;
+      this.size++;
+      return;
+    }
+
+    this.current = new Node<T>(element, this.current);
+    
+    if (this.size() < this.capacity()) {
+      this.size++;
+    } else {
+      Node<T> temp = this.head;
+      this.head = this.current;
+      temp.setPrev(null);
+      temp.setElement(null);
+    }
+  }
+
+  public T peek() {
+    return this.current.element;
+  }
+
+  public T pop() {
+    if (this.isEmpty()) throw new IndexOutOfBoundsException();
+    T elem = this.current.element;
+    this.current = this.current.prev();
+    this.size--;
+    return elem;
+  }
 }
--- src/test/java/com/nicogreco/structures/stack/DropOutStackTest.java
+++ src/test/java/com/nicogreco/structures/stack/DropOutStackTest.java
@@ -1,5 +1,81 @@
 package com.nicogreco.structures.stack;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class DropOutStackTest {
   
+  @Test
+  public void testDropOutStackExists() {
+    try {
+      Class.forName("com.nicogreco.structures.stack.DropOutStack").newInstance();
+    } catch (Exception e) {
+      fail();
+    }
+  }
+
+  @Test
+  public void testInitialCapacitySet() {
+    DropOutStack<Integer> stack = new DropOutStack<>();
+    
+    assertEquals(stack.capacity(), 10);
+  }
+
+  @Test
+  public void testManualCapacitySet() {
+    DropOutStack<Integer> stack = new DropOutStack<>(20);
+    
+    assertEquals(stack.capacity(), 20);
+  }
+
+  @Test
+  public void testSize() {
+    DropOutStack<Integer> stack = new DropOutStack<>();
+
+    stack.push(5);
+    
+    assertEquals(stack.size(), 1);
+  }
+
+  @Test
+  public void testPeek() {
+    DropOutStack<Integer> stack = new DropOutStack<>();
+
+    stack.push(5);
+    
+    assertEquals(stack.peek(), Integer.valueOf(5));
+  }
+
+  @Test
+  public void testThrowsIndexOutOfBounds() {
+    DropOutStack<Integer> stack = new DropOutStack<>();
+
+    try {
+      stack.pop();
+      fail();
+    } catch (IndexOutOfBoundsException e) {}
+  }
+
+  @Test
+  public void testPop() {
+    DropOutStack<Integer> stack = new DropOutStack<>();
+
+    stack.push(5);
+    
+    assertEquals(stack.pop(), Integer.valueOf(5));
+    assertEquals(stack.size(), 0);
+  }
+
+  @Test
+  public void testPushTilOverMax() {
+    DropOutStack<Integer> stack = new DropOutStack<>();
+
+    for (int i = 0; i < 11; i++) {
+      stack.push(i);
+    }
+    
+    assertEquals(stack.pop(), Integer.valueOf(10));
+    assertEquals(stack.pop(), Integer.valueOf(9));
+    assertEquals(stack.size(), 8);
+  }
 }
